8cd3bae321f6f247de14046399a10a893a10863f
marcelo-mf <marcelo.mfilho2804@gmail.com>
2021-11-18 20:49:39 -0300

    informações iniciais e perguntas quase prontos

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -31,3 +31,78 @@ function listarQuizzes(elemento) {
 }
 
 obterQuizzes();
+
+/*-----------------------ESCONDER-MOSTRAR TELAS-------------------*/
+
+function irParaInformacoesBasicas() {
+    const infosBasicas = document.querySelector(".informacoes-basicas");
+    infosBasicas.classList.add("mostrar");
+
+    const listaQuizzes = document.querySelector(".lista-quizzes");
+    listaQuizzes.classList.add("esconder");
+}
+
+function irParaCriarPerguntas() {
+
+    const tituloQuizz = document.querySelector(".titulo-quizz").value;
+    /*const imagemQuizz = document.querySelector(".imagem-quizz").value;*/
+    const qtdPerguntas = document.querySelector(".qtd-perguntas").value;
+    const qtdNiveis = document.querySelector(".qtd-niveis").value;
+    
+    if(tituloQuizz.length >= 20 && tituloQuizz.length <= 65 && parseInt(qtdPerguntas) >= 3 && parseInt(qtdNiveis) >= 2) {
+
+    const infosBasicas = document.querySelector(".informacoes-basicas");
+    infosBasicas.classList.remove("mostrar");
+
+    const listaQuizzes = document.querySelector(".criar-perguntas");
+    listaQuizzes.classList.add("mostrar");
+    } else {
+        alert("Preencha os dados corretamente.");
+    }
+
+    const paginaPerguntas = document.querySelector(".perguntas-mutavel");
+
+    for(let i = 2; i <= parseInt(qtdPerguntas); i++) {
+    paginaPerguntas.innerHTML += `<div class="container-input-reduzido" id="p${i}" onclick="mostrarInputPergunta(this)">
+    <h2 class="h2-container-inputs">Pergunta ${i}</h2>
+    <ion-icon name="create-outline"></ion-icon>
+</div>
+<div class="container-inputs esconder" id="p${i}input">
+    <h2 class="h2-container-inputs">Pergunta ${i}</h2>
+    <input type="text" class="caixa-input" placeholder="Texto da pergunta">
+    <input type="text" class="caixa-input" placeholder="Cor de fundo da pergunta">
+    <h2 class="h2-container-inputs">Resposta correta</h2>
+    <input type="text" class="caixa-input" placeholder="Resposta correta">
+    <input type="text" class="caixa-input" placeholder="URL da imagem">
+    <h2 class="h2-container-inputs">Respostas incorretas</h2>
+    <input type="text" class="caixa-input" placeholder="Resposta incorreta 1">
+    <input type="text" class="caixa-input" placeholder="URL da imagem 1">
+    <div class="separador"></div>
+    <input type="text" class="caixa-input" placeholder="Resposta incorreta 2">
+    <input type="text" class="caixa-input" placeholder="URL da imagem 2">
+    <div class="separador"></div>
+    <input type="text" class="caixa-input" placeholder="Resposta incorreta 3">
+    <input type="text" class="caixa-input" placeholder="URL da imagem 3">
+    <div class="separador"></div>
+</div>`
+    }
+}
+
+function mostrarInputPergunta(inputReduzido) {
+    const mostrarInput = document.querySelector(".mostrar-container-input")
+
+    console.dir(mostrarInput)
+
+    if(mostrarInput !== null) {
+        mostrarInput.classList.remove("mostrar-container-input")
+    }
+
+    inputReduzido.classList.add("esconder")
+
+    const id = inputReduzido.id 
+    const inputAparecer = document.querySelector(`#${id}input`)
+    inputAparecer.classList.add("mostrar-container-input")
+    
+}
+
+
